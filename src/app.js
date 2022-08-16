13dd17eef272aba49ff5b43e342729c09ece9e46
Cleber Pereira Dev <101598282+cleberpereiradev@users.noreply.github.com>
2022-08-16 12:18:09 -0300

    <feat> atualização de código e conexão ao BD
    Utilizando o sistema NoSQL MongoDB, utilizando a biblioteca Mongoose.

--- src/app.js
+++ src/app.js
@@ -1,22 +1,32 @@
-import express from "express";
+import express, { json } from "express";
+import db from './config/dbConnect.js';
+
+db.on("error", console.log.bind("Conexão com o BD falhou!"));
+db.once("open", () =>{
+    console.log("Conexão com o BD bem sucedida!");
+} )
 
 
 const app = express();
 
 app.use(express.json());
 
-const receitas = [
-    {
-        "id": 1,
-        "descrição": "Salário",
-        "valor": 1500
-    },
-    {
-        "id": 2,
-        "descrição": "Freela",
-        "valor": 500
-    }
-];
+
+//array de teste
+// const receitas = [
+//     {
+//         "id": 1,
+//         "descrição": "Salário",
+//         "valor": 1500
+//     },
+//     {
+//         "id": 2,
+//         "descrição": "Freela",
+//         "valor": 500
+//     }
+// ];
+
+
 
 //acessando a primeira pagina
 
@@ -44,16 +54,78 @@ app.get('/receitas/:id', (req, res) => {
 })
 
 //alterar atributos
-app.put('receitas/:id', (req, res) => {
-    let index = buscaLivro(req.params.id);
-    receitas[index].valor = (req.body.id);
+app.put('/receitas/:id', (req, res) => {
+    let index = buscaReceita(req.params.id);
+    receitas[index].valor = req.body.valor;
     res.json(receitas);
+})
+
+//deletar itens por id
+
+app.delete('/receitas/:id', (req, res) =>{
+    let {id} = req.params;
+    let index = buscaReceita(id);
+    receitas.splice(index,1);
+    res.send(`O item com id ${id}, foi removido com sucesso`);
+
 })
 
 //função de procurar indíce no array
 
 function buscaReceita(id){
-    return receitas.findIndex(receita => receita.id == id)
+    return receitas.findIndex(receita => receita.id == id);
+}
+
+
+
+
+
+const despesas = [
+    {
+        "id": 3,
+        "descricao": "conta de agua",
+        "valor": 120
+    },
+    {
+        "id": 4,
+        "descricao": "conta de luz",
+        "valor": 150
+    }
+];
+
+app.get('/despesas/', (req, res) => {
+
+    res.json(despesas);
+
+})
+
+app.get('/despesas/:id', (req, res) => {
+    let index = buscaDespesa(req.params.id);
+    res.json(despesas[index]);
+})
+
+app.post('/despesas', (req, res) => {
+    despesas.push(req.body);
+    res.status(200).send('Despesa adicionada com sucesso');
+
+})
+
+app.delete('/despesas/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaDespesa(id);
+    despesas.splice(index,1);
+    res.send(`O item com ${id}, foi removido com sucesso`);
+})
+
+app.put('/despesas/:id', (req, res) => {
+    let index = buscaDespesa(req.params.id);
+    despesas[index].valor = req.body.valor;
+    res.json(despesas);
+})
+
+
+function buscaDespesa(id){
+    return despesas.findIndex(despesa => despesa.id == id);
 }
 
 export default app;
